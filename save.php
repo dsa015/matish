e69aa403f5063d7725d7a6009e78addb1aabe062
dsa015 <91134700+dsa015@users.noreply.github.com>
2022-11-17 20:17:56 +0100

    update save.php

--- save.php
+++ save.php
@@ -79,6 +79,6 @@ if ($_SESSION['condnum']!=$condnum) {$_SESSION['condnum']=$condnum;}
 /* Redirect to a different page in the current directory that was requested */
 $host  = $_SERVER['HTTP_HOST'];
 $uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
-header("Location: http://$host$uri/$nextURL");
+header("Location: https://$host$uri/$nextURL");
 exit;
 ?>
